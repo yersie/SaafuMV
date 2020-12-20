363ff67d94b134ac8adb061bdcd239de60493329
unknown <yersih@outlook.com>
2020-12-20 23:30:59 +0500

    modified:   lib/helper/validator.dart

--- lib/helper/validator.dart
+++ lib/helper/validator.dart
@@ -1,13 +1,13 @@
 import 'dart:core';
 
 class Validator {
-  static final emailRegExp = new RegExp(
+  static final _emailRegExp = new RegExp(
     r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$",
     caseSensitive: false,
     multiLine: false,
   );
 
   static bool validateEmail(email) {
-    return !emailRegExp.hasMatch(email);
+    return !_emailRegExp.hasMatch(email);
   }
 }
